41967077d93b550b7db9f87d8f904b168dbc2341
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2017-01-29 11:45:43 +0530

    Deleting non-java files now

--- deletenonjava.py
+++ deletenonjava.py
@@ -5,6 +5,6 @@ project_folder = sys.argv[1]
 
 for root, subdirs, files in os.walk(project_folder):
 	for f in files:
-		if ".java" in f:
+		if ".java" not in f:
 			file_path = os.path.join(root, f)
 			os.remove(file_path)
