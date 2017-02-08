5d40b011fca84e043d7180754a04a78c94365cf9
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2017-02-09 00:07:43 +0530

    Basic CNN utilities

---
+++ implementation/utils.py
@@ -0,0 +1,73 @@
+#Basic utility functions used with CNNs
+
+import numpy as np
+import pandas as pd
+
+
+def init_weight_and_bias(M1, M2):
+    W = np.random.randn(M1, M2) / np.sqrt(M1 + M2)
+    b = np.zeros(M2)
+    return W.astype(np.float32), b.astype(np.float32)
+
+
+def init_filter(shape, poolsz):
+    w = np.random.randn(*shape) / np.sqrt(np.prod(shape[1:]) + shape[0]*np.prod(shape[2:] / np.prod(poolsz)))
+    return w.astype(np.float32)
+
+
+def relu(x):
+    return x * (x > 0)
+
+
+def sigmoid(A):
+    return 1 / (1 + np.exp(-A))
+
+
+def softmax(A):
+    expA = np.exp(A)
+    return expA / expA.sum(axis=1, keepdims=True)
+
+
+def sigmoid_cost(T, Y):
+    return -(T*np.log(Y) + (1-T)*np.log(1-Y)).sum()
+
+
+def cost(T, Y):
+    return -(T*np.log(Y)).sum()
+
+
+def cost2(T, Y):
+    # same as cost(), just uses the targets to index Y
+    # instead of multiplying by a large indicator matrix with mostly 0s
+    N = len(T)
+    return -np.log(Y[np.arange(N), T]).mean()
+
+
+def error_rate(targets, predictions):
+    return np.mean(targets != predictions)
+
+
+def y2indicator(y):
+    N = len(y)
+    K = len(set(y))
+    ind = np.zeros((N, K))
+    for i in xrange(N):
+        ind[i, y[i]] = 1
+    return ind
+
+def crossValidation(model, X, Y, K=5):
+    # split data into K parts
+    X, Y = shuffle(X, Y)
+    sz = len(Y) / K
+    errors = []
+    for k in xrange(K):
+        xtr = np.concatenate([ X[:k*sz, :], X[(k*sz + sz):, :] ])
+        ytr = np.concatenate([ Y[:k*sz], Y[(k*sz + sz):] ])
+        xte = X[k*sz:(k*sz + sz), :]
+        yte = Y[k*sz:(k*sz + sz)]
+
+        model.fit(xtr, ytr)
+        err = model.score(xte, yte)
+        errors.append(err)
+    print "errors:", errors
+    return np.mean(errors)
