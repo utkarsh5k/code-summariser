269e0048fa3b9e5245447d628e383bd2de30570e
ykl7 <yklal95@gmail.com>
2017-03-03 00:49:39 +0530

    recurrent convolution data formatting

--- implementation/formatting_tokens.py
+++ implementation/formatting_tokens.py
@@ -218,4 +218,29 @@ class FormatTokens:
         return naming.conv_data(names[idxs[:lim]], code[idxs[:lim]], names_cx_size, min_code_size),\
                 naming.conv_data(names[idxs[lim:]], code[idxs[lim:]], names_cx_size, min_code_size), naming
 
+    def data_in_rec_conv_format(self, input_file, min_code_size):
+        names, code, original_names = self.__read_file(input_file)
+        return self.rec_conv_data(names, code, min_code_size), original_names
+
+    """
+    looks similar to convolution data formatting, but it really isn't
+    """
+    def rec_conv_data(self, names, code, sentence_padding):
+        assert len(names) == len(code), (len(names), len(code), code.shape)
+        name_targets = []
+        sentences = []
+        padding = [self.all_tokens_dictionary.is_id_or_is_unknown(self.NONE)]
+        for i, name in enumerate(names):
+            sentence = [self.all_tokens_dictionary.is_id_or_is_unknown(token) for token in code[i]]
+            if sentence_padding % 2 == 0:
+                sentence = padding * (sentence_padding / 2) + sentence + padding * (sentence_padding / 2)
+            else:
+                sentence = padding * (sentence_padding / 2 + 1) + sentence + padding * (sentence_padding / 2)
+            name_tokens = [self.all_tokens_dictionary.is_id_or_is_unknown(token) for token in name]
+            name_targets.append(np.array(name_tokens, dtype=np.int32))
+            sentences.append(np.array(sentence, dtype=np.int32))
+        name_targets = np.array(name_targets, dtype=np.object)
+        sentences = np.array(sentences, dtype=np.object)
+        return name_targets, sentences
+
 
