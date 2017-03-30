988f1c2cb1c01756da3036935087526b622f032a
ykl7 <yklal95@gmail.com>
2017-03-31 03:35:06 +0530

    add script to run copy conv rec model to every file

---
+++ autorun.py
@@ -0,0 +1,23 @@
+import os
+
+os.system("python copy_conv_rec_learner.py ../Data/json/libgdx_train_methodnaming_shuffled.json 1 16 ../Data/json/libgdx_test_methodnaming_shuffled.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/cassandra_train_methodnaming.json 1 16 ../Data/json/cassandra_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/elasticsearch_train_methodnaming.json 1 16 ../Data/json/elasticsearch_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/gradle_train_methodnaming.json 1 16 ../Data/json/gradle_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/hadoop-common_train_methodnaming.json 1 16 ../Data/json/hadoop-common_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/hibernate-orm_train_methodnaming.json 1 16 ../Data/json/hibernate-orm_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/intellij-community_train_methodnaming.json 1 16 ../Data/json/intellij-community_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/liferay-portal_train_methodnaming.json 1 16 ../Data/json/liferay-portal_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/presto_train_methodnaming.json 1 16 ../Data/json/presto_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/spring-framework_train_methodnaming.json 1 16 ../Data/json/spring-framework_test_methodnaming.json")
+
+os.system("python copy_conv_rec_learner.py ../Data/json/wildfly_train_methodnaming.json 1 16 ../Data/json/wildfly_test_methodnaming.json")
